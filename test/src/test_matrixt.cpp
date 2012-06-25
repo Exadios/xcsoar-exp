79bbce233b6d4cadd657fe7dd0ce78bfbf7d431f
Peter F Bradshaw <pfb@exadios.com>
2012-06-25 15:12:56 +0800

    test_matrixt: Added tests.

--- test/src/test_matrixt.cpp
+++ test/src/test_matrixt.cpp
@@ -22,8 +22,14 @@
 
 #include "Math/fixed.hpp"
 #include "Math/MatrixT.hpp"
+#include <iostream>
 
-static void
+bool test_basic_ctor();
+bool test_addition();
+bool test_subtraction();
+bool test_multiplication();
+
+bool
 test_basic_ctor()
   {
   MatrixT<fixed, 3, 3> A;
@@ -31,12 +37,102 @@ test_basic_ctor()
   MatrixT<fixed, 3, 3> C;
   B = A;
   C = A + B;
+  return true;
+  }
+
+bool
+test_addition()
+  {
+  MatrixT<size_t, 3, 3> A;
+  MatrixT<size_t, 3, 3> B;
+  MatrixT<size_t, 3, 3> C;
+  size_t i, j;
+
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++)
+      {
+      A[i][j] = j + 3 * i;
+      B[i][j] = j + 3 * i + 10;
+      }
+  C = A + B;
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++)
+      {
+      if (C[i][j] != j + 3 * i + j + 3 * i + 10)
+        return false;
+      }
+  return true;
+  }
+
+bool
+test_subtraction()
+  {
+  MatrixT<size_t, 3, 3> A;
+  MatrixT<size_t, 3, 3> B;
+  MatrixT<size_t, 3, 3> C;
+  size_t i, j;
+
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++)
+      {
+      A[i][j] = j + 3 * i + 10;
+      B[i][j] = j + 3 * i;
+      }
+  C = A - B;
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++)
+      {
+      if (C[i][j] != j + 3 * i + 10 - (j + 3 * i))
+        return false;
+      }
+  return true;
+  }
+
+bool
+test_multiplication()
+  {
+  MatrixT<size_t, 3, 3> A;
+  MatrixT<size_t, 3, 3> B;
+  MatrixT<size_t, 3, 3> C;
+  size_t i, j, k;
+
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++)
+      {
+      A[i][j] = j + 3 * i;
+      B[i][j] = j + 3 * i;
+      }
+  C = A * B;
+
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++)
+      std::cout << "C[" << i << "][" << j << "] = " << C[i][j] << std::endl;
+
+  for (i = 0; i < 3; i++)
+    for (j = 0; j < 3; j++)
+      {
+      size_t acc = 0;
+      for (k = 0; k < 3; k++)
+	acc += (3 * i + k) * (3 * k + j);
+      std::cout << "C[" << i << "][" << j << "] = " << C[i][j] << ", acc = " << acc << std::endl;
+      if (C[i][j] != acc)
+        return false;
+      }
+
+  return true;
   }
 
 int
 main(int argc, const char *argv[])
   {
-  test_basic_ctor();
+  if (test_basic_ctor() == false)
+    return 1;
+  if (test_addition() == false)
+    return 2;
+  if (test_subtraction() == false)
+    return 3;
+  if (test_multiplication() == false)
+    return 4;
 
   return 0;
   }
