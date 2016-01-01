1dceeb5e1f11379bc1eefc866a6fc2b5ec7eea9e
Peter F Bradshaw <pfb@exadios.com>
2016-01-01 10:10:10 +0800

    Australian Zone: Initial commit. Unit tests need work.

--- src/Task/Serialiser.cpp
+++ src/Task/Serialiser.cpp
@@ -27,6 +27,7 @@
 #include "Task/ObservationZones/LineSectorZone.hpp"
 #include "Task/ObservationZones/KeyholeZone.hpp"
 #include "Task/ObservationZones/AnnularSectorZone.hpp"
+#include "Task/ObservationZones/AustralianKeyholeZone.hpp"
 #include "XML/DataNode.hpp"
 #include "Compiler.h"
 
@@ -107,6 +108,17 @@ Visit(WritableDataNode &node, const AnnularSectorZone &data)
   node.SetAttribute(_T("inner_radius"), data.GetInnerRadius());
 }
 
+//------------------------------------------------------------------------------
+static void
+Visit(WritableDataNode &node, const AustralianKeyholeZone &data)
+  {
+  node.SetAttribute(_T("type"),         _T("AustralianKeyholeZone"));
+  node.SetAttribute(_T("radius"),       data.GetRadius());
+  node.SetAttribute(_T("inner_radius"), data.GetInnerRadius());
+  node.SetAttribute(_T("start_radial"), data.GetStartRadial());
+  node.SetAttribute(_T("end_radial"),   data.GetEndRadial());
+  }
+
 static void
 Visit(WritableDataNode &node, const LineSectorZone &data)
 {
@@ -170,6 +182,10 @@ Serialise(WritableDataNode &node, const ObservationZonePoint &data)
 
   case ObservationZone::Shape::ANNULAR_SECTOR:
     Visit(node, (const AnnularSectorZone &)data);
+    break;
+
+  case ObservationZone::Shape::AUSTRALIAN_KEYHOLE:
+    Visit(node, (const AustralianKeyholeZone &)data);
     break;
 
   case ObservationZone::Shape::SYMMETRIC_QUADRANT:
