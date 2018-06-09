75ab5d78d4f404be30c9d93172b472d40751b8c0
Peter F Bradshaw <pfb@exadios.com>
2018-06-09 09:44:04 +0800

    Build: Added temporary-flags.cmake
    This file is used to hold temporary compiler options. By temporary we
    mean applicable to this checkout.

---
+++ temporary-flags.cmake
@@ -0,0 +1,10 @@
+# These are compiler flags that are temporarily required to compile the code
+# at this checkout. The most likely reason that is that the compiler or some
+# other aspect of the build environment has changed since the time that this
+# current checkout was written.
+#
+# These flags should be dispensed with ASAP.
+
+set(temporary_cxx_flags "-Wno-error=unused-parameter -Wno-error=deprecated-declarations -Wno-error=shift-negative-value")
+
+set(temporary_c_flags "")
