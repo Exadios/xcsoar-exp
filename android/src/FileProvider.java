9c943c7c597a9174ab35470c42b2ffe3f35d9c8a
Peter F Bradshaw <pfb@exadios.com>
2021-11-24 17:36:06 +0800

    More transition to narroginglidingclub.org.au
    This commit does a SEGV early in NativeView_initializeNative
    (src/Android/Main.cpp). The exact message is:
    
    "Fatal signal 11 (SIGSEGV), code 1, fault addr 0x0 in tid 3940 (NativeMain)"
    
    Looks like a nullptr.

--- android/src/FileProvider.java
+++ android/src/FileProvider.java
@@ -20,7 +20,7 @@
 }
 */
 
-package org.xcsoar;
+package au.org.narroginglidingclub.xcsoar;
 
 import java.io.File;
 import java.io.FileNotFoundException;
--- src/Android/NativeView.cpp
+++ src/Android/NativeView.cpp
@@ -51,7 +51,7 @@ jmethodID NativeView::bitmapConfigValueOf_method;
 void
 NativeView::Initialise(JNIEnv *env)
 {
-  cls.Find(env, "org/narroginglidingclub/xcsoar/NativeView");
+  cls.Find(env, "au/org/narroginglidingclub/xcsoar/NativeView");
 
   textureNonPowerOfTwo_field =
     env->GetStaticFieldID(cls, "textureNonPowerOfTwo", "Z");
