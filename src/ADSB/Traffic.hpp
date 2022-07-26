aa088b73192572aa7d8d4cfd6ea0aeab56bcc180
Peter F Bradshaw <pfb@exadios.com>
2022-07-26 15:31:58 +0800

    Squash me

--- src/ADSB/Traffic.hpp
+++ src/ADSB/Traffic.hpp
@@ -2,11 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-<<<<<<< HEAD
   Copyright (C) 2000-2022 The XCSoar Project
-=======
-  Copyright (C) 2000-2021 The XCSoar Project
->>>>>>> origin/Issue-5-on-master
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
