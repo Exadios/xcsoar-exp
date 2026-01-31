e59ff92254dd8cc7145caf30084faf2ebcbcc49b
Peter F Bradshaw <pfb@exadios.com>
2026-01-31 21:02:03 +0800

    Update copyright to 2026

--- src/Tracking/SkyLines/Protocol.hpp
+++ src/Tracking/SkyLines/Protocol.hpp
@@ -1,6 +1,6 @@
 /*
  * XCSoar Glide Computer - http://www.xcsoar.org/
- * Copyright (C) 2000-2025 The XCSoar Project
+ * Copyright (C) 2000-2026 The XCSoar Project
  * A detailed list of copyright holders can be found in the file "AUTHORS".
  *
  * Redistribution and use in source and binary forms, with or without
